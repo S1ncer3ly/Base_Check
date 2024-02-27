47ad85ea59b77be5572d86ccb7715d153a3f690f
S1ncer3ly <siddharthchaudhary611@gmail.com>
2024-02-27 21:05:50 +0530

    added cube function to square class

--- square.cpp
+++ square.cpp
@@ -11,11 +11,17 @@ public:
         cout<<"Enter a Number :";
         cin>>a;
         square(a);
+        cube(a);
     }
     void square(int x)
     {
         x=a*a;
         cout<<"The Square of the number "<<a<<" is : "<<x;
+    }
+    void cube (int x)  // Cube Function using square class
+    {
+        x= a*a*a;
+        cout<<"\nThe Cube of the number "<<a<<" is : "<<x;
     }
 
 
